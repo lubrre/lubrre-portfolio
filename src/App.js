d1fa988cc3f3bedba9ba4188a5bd048d78b38b79
lubrre <barrere.lucas@icloud.com>
2022-08-18 18:25:54 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+// import style
+import "./App.css";
+
+// react router
+import { Routes, Route } from "react-router-dom";
+
+// import navbar & footer
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// import pages
+import Home from "./components/pages/Home";
+import Project from "./components/pages/Projects";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import PorscheRentalExperience from "./components/pages/projects/PorscheRentalExperience"
+import TaxiAllan from "./components/pages/projects/TaxiAllan"
+import RoadArt from "./components/pages/projects/RoadArt"
+import WeeklyDesign from "./components/pages/projects/WeeklyDesign"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/porscherentalexperience" element={<PorscheRentalExperience />} />
+        <Route path="/taxiallan" element={<TaxiAllan />} />
+        <Route path="/roadart" element={<RoadArt />} />
+        <Route path="/weeklydesign" element={<WeeklyDesign />} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
